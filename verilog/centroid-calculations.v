43c89d7762ef4dc02d047be1ebcb33e9b357b974
Michael Grillo <mjg397@cornell.edu>
2026-03-23 16:29:34 -0400

    Comment out code and add optimization notes

---
+++ verilog/centroid-calculations.v
@@ -0,0 +1,7 @@
+//when completed[i]:
+//  do the intensity[i] division
+///  x_intensity[i] * 1/intensity[i]
+//  y_intensity[i] * 1/intensity[i]
+
+//this can hopefully be done in a way, combinationally. thinking about possibly approximations aprroacher or lookup tables
+// stephen will be looking into latency optimization for this
