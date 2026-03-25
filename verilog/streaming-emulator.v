2943fcd4b3326e6b6d7f6a6173b33ff17fd06307
Michael Grillo <mjg397@cornell.edu>
2026-03-25 12:22:09 -0400

    CMOS Style emulator, I wonder if it compiles ¯\_(ツ)_/¯
    Added parameters for frame size and blanking, implemented state machine for pixel input, and initialized memory from hex file.

--- verilog/streaming-emulator.v
+++ verilog/streaming-emulator.v
@@ -1,30 +1,92 @@
 // this emulates the streaming property of the shack-hartmann wavefront sensor from a preloaded image into FPGA memroy
 // this intends to allow for our shack-hartmann processing pipeline to have an accurate representation of incoming data
-// currently this verilog is pseudo and does not synthesize
-parameter WIDTH
-parameter HEIGHT
+// I'm not sure if it syntheizes i havent tried yet :\
+parameter HSIZE  = 256; // Horozontal Frame Size
+parameter VSIZE  = 256; // Vertical Frame Size
+parameter HBLANK = 4;   // Horozontal Blanking
+parameter VBLANK = 152; // Vertical Blanking
 
-module (parameter N) Shack_Hartmann_Emulator ( // N is the number of pixels per clk cycle
-    input wire clk,
-    input wire rst,
-    output reg [26:0] raw_data [N:0]
-);
-  // memory is read from given from python spi code
-  reg mem_idx = 0;
+output reg fv;
+output reg lv;
+output reg frame_complete;
+output wire valid;
 
-  always @(posedge_clk) begin
-    if (rst) begin
-      mem_idx <= 0;
-      data_out <= 0;
-    end
-    else if (mem_idx == memory_size) begin
-      mem_idx <= 0;
-      complete_frame <= 1;
-    end
-    else begin
-      mem_idx <= mem_idx + N;
-      raw_data <= memory[mem_idx:N];
-    end
+assign valid = fv & lv;
+
+reg [$clog2(HSIZE)-1:0] line_counter
+reg [$clog2(VSIZE)-1:0] row_counter
+
+// Instantiate into ROM
+reg [7:0] mem [0:65535];
+
+initial begin
+    $readmemh("image.hex", mem);
+end
+
+
+// state machine for pixel input
+
+localparam STATE_FRAME_INIT          = 2'b00;
+localparam STATE_ACTIVE_FRAME;       = 2'b01;
+localparam STATE_HOROZONTAL_BLANKING = 2'b10;
+localparam STATE_VERTICAL_BLANKING   = 2'b11;
+
+reg [1:0] state;
+
+always @(posedge clk) begin
+  if (reset == 1) begin
+    state <= STATE_FRAME_INIT;
+    line_counter <= 0;
+    row_counter <= 0;
+    h_blank_counter <= 0;
+    v_blank_counter <= 0;
+    lv <= 0;
+    fv <= 0;
   end
 
-endmodule
+  else begin
+    case (state) begin
+
+      STATE_FRAME_INIT: begin
+        fv <= 1;
+        line_counter <= 0;
+        row_counter <= 0;
+        state <= STATE_ACTIVE_FRAME
+      end
+
+      STATE_ACTIVE_FRAME: begin
+        data <= mem[row_counter * HSIZE + line_counter];
+        line_counter <= line_counter + 1;
+        lv <= 1;
+        if ((line_counter == HSIZE - 1) && (row_counter == VSIZE - 1)) begin
+          STATE <= STATE_VERTICAL_BLANKING;
+          frame_complete <= 1;
+        end
+        else if (line_counter == HSIZE - 1) begin
+          state <= STATE_HOROZONTAL_BLANKING
+        end
+      end
+                 
+      STATE_HOROZONTAL_BLANKING: begin
+        lv <= 0;
+        line_counter <= 0;
+        h_blank_coutner = h_blank_counter + 1;
+        if (h_blank_counter == HBLANK - 1)
+          h_blank_counter <= 0;
+          row_counter <= row_counter + 1
+          state <= STATE_ACTIVE_FRAME;
+        end
+      end
+
+      STATE_VERTICAL_BLANKING: begin
+        fv <= 0;
+        lv <= 0;
+        line_counter <= 0;
+        row_counter <= 0;
+        v_blank_counter <= v_blank_counter + 1;
+        if (v_blank_counter == VBLANK - 1) begin
+          v_blank_counter <= 0;
+          state <= STATE_FRAME_INIT
+        end
+      end
+    endcase
