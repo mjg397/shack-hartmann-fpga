c46199c7ddab35926b639b8326e97619acfa2a2b
Michael Grillo <mjg397@cornell.edu>
2026-03-30 21:51:12 -0400

    Enhance matrix accumulation with FSM and parameters
    Refactor matrix accumulation logic and add FSM for processing states.

--- verilog/matrix_accumulation.v
+++ verilog/matrix_accumulation.v
@@ -1,20 +1,46 @@
-// think about doing an optimized least squares caluclation
-// have an fsm do this all starting to the end , with 3 states, a reset stage, an intermediate stage, and a completed stage, which then returns something to trigger that the processing has completed
-take the matrix, at each point where the waveslopes are calculated, can have matrix get multiplied and accumulated
+parameter NUM_ZERNIKE = 10;
 
-input wire [17:0] slope_vector    [0:2NUM_*N];
-output wire [17:0] zernike_outputs [0:NUM_ZERNIKES-1];
+input wire clk;
+input wire rst;
+input wire slopes_ready
+input wire [17:0] slope_vector [0:2NUM_*N];
+output reg zernike_complete;
+output reg [17:0] zernike_outputs [0:NUM_ZERNIKES-1];
 
-reg [17:0] A [0:NUM_ZERNIKE-1][0:2*NUM_SUAP*NUM_SUBAP]; // i think this is 92160 / 120000 of our memory on our registers 💀💀💀💀
+wire [17:0] A [0:NUM_ZERNIKE-1][0:2*NUM_SUAP*NUM_SUBAP]; // i think this is 92160 / 120000 of our memory on our registers 💀💀💀💀
 
 reg [17:0] zernike_accumulation [0:NUM_ZERNIKE-1];
-generate
-for(int i = 0; i < NUM_ZERNIKES; i++) begin
-  zernike_accumulation[i] += 
-counter <= counter + 1;
 
-when counter == NUM_SUBAP*NUM_SUBAP:
-  counter <= 0;
-  zernike_complete <= 1;
-  assign zernike_outputs = zernike_accumulation;
-end
+
+assign slope_x <= slope_vector[slope_idx_x];
+assign slope_y <= slope_vector[slope_idx_y];
+/////////////////
+genvar i;
+
+// generate each individual accumulator for each zernike polynomial
+generate
+  for(int i = 0; i < NUM_ZERNIKES; i++) begin : accumulator
+    always @ (posedge clk) begin
+      if (rst) begin
+        zernike_accumulation[i] <= 0;
+        slope_idx_x <= 0;
+        slope_idx_y <= 1;
+      end
+      else begin
+        if slopes_ready begin
+          zernike_accumulation[i] <= (A[i][slope_x_idx] * slope_x) + (A[i][slope_y_idx] * slope_y)
+          slope_x <= slope_y + 2;
+          slope_y <= slope_y + 2;
+          if (slope_x == NUM_SUBAP*NUM_SUBAP) begin // check to ensure this goes off on correct conditions
+            slope_idx_x <= 0;
+            slope_idx_y <= 1;
+            zernike_accumulation[i] <= 0;
+            zernike_outputs[i] <= zernike_accumulation[i];
+            zernike_complete <= 1;
+          end
+        end
+      end
+  end
+endgenerate
+
+////////////////////
