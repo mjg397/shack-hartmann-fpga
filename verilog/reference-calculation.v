b6264090f6aa195af82d015d45575af2402788f2
Michael Grillo <mjg397@cornell.edu>
2026-03-30 22:00:56 -0400

    Refactor reference_calculation module structure
    Refactor reference_calculation module to include clock and reset signals, and update slope calculations.

--- verilog/reference-calculation.v
+++ verilog/reference-calculation.v
@@ -1,15 +1,26 @@
-module reference_calculation
-  input wire [17:0] x_intensity [N*N:0]
-  input wire [17:0] y_intensity
-  output wire [17:0] x_slopes [N*N:0]
-  output wire  [17:0] y_slopes [N*N:0]
-  output wire [17:0] slope_vector[2N*N:0]
+module reference_calculation (
+  input wire        clk,
+  input wire        rst,
+  input wire [17:0] x_intensity,
+  input wire [17:0] y_intensity,
+  input wire []     centroid [0:NUM_CENTROids],
+  output reg [17:0] x_slope,
+  output reg [17:0] y_slope
+);
 
-
-  generate // generate a bunch of subtractors for the centroids
-  for i in range(N^2):
-    x_slopes[i] = x_intensity[i] - x_ref[i]
-    y_slopes[i] = y_intensity[i] - y_ref[i]
-    slope_vector[2i] = x_slopes[i]
-    slope_vector[2i+1] = y_slopes[i]
+  always @(posedge clk) begin
+  if (reset) begin
+    x_slope <= 0;
+    y_slope <= 0;
+    slope_vector <= 0;
+  end
+  else
+    if frame_valid begin
+        x_slopes[i] = x_intensity[i] - x_ref[i]
+        y_slopes[i] = y_intensity[i] - y_ref[i]
+        slope_vector[2i] = x_slopes[i]
+        slope_vector[2i+1] = y_slopes[i]
+    else
+      x_slopes <= 0;
+      y_slopes <= 0;
 endmodule
