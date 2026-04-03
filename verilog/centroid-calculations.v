6cf3e63561bf0ecbc92f7677185dca2eecb5033b
Michael Grillo <mjg397@cornell.edu>
2026-04-03 13:20:28 -0400

    Delete verilog/centroid-calculations.v

--- verilog/centroid-calculations.v
+++
@@ -1,8 +0,0 @@
-//this file will be deleted soon due to not being necessary, this file is an outdated version of centroid-division.v
-//when completed[i]:
-//  do the intensity[i] division
-///  x_intensity[i] * 1/intensity[i]
-//  y_intensity[i] * 1/intensity[i]
-
-//this can hopefully be done in a way, combinationally. thinking about possibly approximations aprroacher or lookup tables
-// stephen will be looking into latency optimization for this
