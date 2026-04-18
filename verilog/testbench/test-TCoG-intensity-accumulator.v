cf181f115c10ba803972ea2e13670feaf4630da0
Michael Grillo <mjg397@cornell.edu>
2026-04-17 21:58:39 -0400

    Refactor TCoG intensity accumulator for square root params
    Refactor TCoG intensity accumulator module to use square root parameters and adjust internal logic for pixel counting and intensity accumulation.

--- verilog/testbench/test-TCoG-intensity-accumulator.v
+++ verilog/testbench/test-TCoG-intensity-accumulator.v
@@ -23,65 +23,108 @@ endmodule
 
 
 
-module TCoG-intensity-accumulator #(
-  parameter NUM_SUBAPETURES = 16,
-  parameter NUM_PIXELS_SUBAPETURE = 16,
+module TCoG_intensity_accumulator #(
+  parameter NUM_SUBAPETURES_SQRT = 16,
+  parameter NUM_PIXELS_SUBAPETURE_SQRT = 16,
   parameter NUM_CENTROIDS = 9
 )(
   input wire clk,
   input wire reset,
   input wire valid,
   input wire [7:0] data_in,
-  output reg        subap_valid [NUM_SUBAP*NUM_SUBAP-1:0] = {(NUM_SUBAP*NUMSUBAP)1'b0}, \\set them default invalid until the streaming comes in (this allows for us to have feature of early complete detection)
-  output reg [15:0] intensity   [NUM_SUBAP*NUM_SUBAP-1:0],
-  output reg [15:0] x_intensity [NUM_SUBAP*NUM_SUBAP-1:0],
-  output reg [15:0] y_intensity [NUM_SUBAP*NUM_SUBAP-1:0],
-  output reg [$clog2(NUM_SUBAP)-1:0] subap_col,
-  output reg [$clog2(NUM_SUBAP)-1:0] subap_row
+  output reg full_frame_complete;
+  output reg [$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] subapetures_completed // indexed with i=1 corresponding to centroid 0, 0 completed
+  output reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] intensity,
+  output reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] x_intensity,
+  output reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] y_intensity,
 );
 
-reg [$clog2(NUM_SUBAP)-1:0] subap_col;
-reg [$clog2(NUM_SUBAP)-1:0] subap_row;
+wire [$clog2(NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT)-1:0] subap_idx;
+wire last_pixel_h = (count_pixel_h == NUM_PIXELS_SUBAPETURE-SQRT-1);
+wire last_pixel_v = (count_pixel_v == NUM_PIXELS_SUBAPETURE_SQRT-1);
+wire last_subap_col = (subap_col == NUM_SUBAPETURE_SQRT-1);
+wire last_subap_row = (subap_row == NUM_SUBAPETURE_SQRT-1);
 
-assign subap_idx = subap_row * NUM_SUBAP_X + subap_col;
+reg [$clog2(NUM_SUBAPETURES_SQRT)-1:0] subap_col;
+reg [$clog2(NUM_SUBAPETURES_SQRT)-1:0] subap_row;
+reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] i   [NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1:0];
+reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] x_i [NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1:0];
+reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] y_i [NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1:0];
 
-// control logic for TCoG
-always @(posedge clk) begin
-  if (reset) begin
+assign subap_idx = (subap_row * NUM_SUBAPETURES_SQRT + subap_col);
 
-  else begin
-    if valid begin
-      count_pixel_h <= count_pixel_h + 1;
-      if (count_pixel_h == NUM_PIXELS_SUBAP) begin
+//  TCoG indexing control logic
+  always @(posedge clk) begin
+    if (reset) begin
+      subapetures_completed <= 0;
+      subap_col <= 0;
+      subap_row <= 0;
+    end
+    else if (valid) begin
+      full_frame_complete <= 0;
+
+      if (last_pixel_h) begin
         count_pixel_h <= 0;
-        subap_col <= subap_col + 1;
-        if (subap_col_v == NUM_SUBAP) begin
-          subap_col <= 0;
+      end else begin
+        count_pixel_h <= count_pixel_h + 1;
+      end
+
+      if (last_pixel_h) begin
+        if (last_pixel_v) begin
+          count_pixel_v <= 0;
+        end else begin
           count_pixel_v <= count_pixel_v + 1;
-          if (subap_pixel_v == NUM_PIXELS_SUBAP) begin
-            subap_row <= subap_row + 1;
-            subap_col <= 0;
-          end
-          if (subap_row == NUM_SUBAP && subap_col == NUM_SUBAP && count_pixel_h == NUM_PIXELS_SUBAP && count_pixel_v == NUM_PIXELS_SUBAP) begin
-            complete_frame <= 1;
-          end
         end
       end
-  end
 
-  // accumulation of pixel data
+      if (last_pixel_h && last_pixel_v) begin
+          if (last_subap_col)
+              subap_col <= 0;
+          else
+              subap_col <= subap_col + 1;
+       end
+
+      if (last_pixel_h && last_pixel_v && last_subap_col) begin
+        if (last_subap_row) begin
+          subap_row <= 0;
+          full_frame_complete <= 1;
+        end else begin
+          subap_row <= subap_row + 1;
+        end
+      end
+    end
+  end
+  
+  // Completed subapeture indication
   always @(posedge clk) begin
-  if (reset) begin
+    if (reset) begin
+      intensity <= 0;
+      x_intensity <= 0;
+      y_intensity <= 0;
+    end else begin
+      if (last_pixel_h && last_pixel_v) begin
+        subapetures_completed <= subapetures_completed + 1;
+        intensity <= i[subap_idx];
+        x_intensity <= x_i[subap_idx];
+        y_intensity <= y_i[subap_idx];
+      end
+    end
   end
-  else begin
-    intensity[subap_row * NUM_SUBAP + subap_col] <= data_in
-    x_intensity[subap_row * NUM_SUBAP + subap_col] <= data_in*count_pixel_h
-    y_intensity[subap_row * NUM_SUBAP + subap_col] <= data_in*count_pixel_y
+
+
+  // Intensity accumulation of valid pixel data
+  always @(posedge clk) begin
+    if (reset) begin
+      for (i = 0; i < NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1; i = i + 1) begin
+              i[i]   <= 8'b0; 
+              x_i[i] <= 8'b0;
+              y_i[i] <= 8'b0;
+      end
+    end
+    else if (valid) begin
+      i[subap_idx]   <= i[subap_idx] + data_in;
+      x_i[subap_idx] <= x_i[subap_idx] + data_in*count_pixel_h;
+      y_i[subap_idx] <= x_i[subap_idx] + data_in*count_pixel_y;
+    end
   end
-
-  // valid data handling
-
-  if at row NUM_SUBAP-1 and
-    if (valid && ()) // if it is valid pixel and is in the frame
-
 endmodule
