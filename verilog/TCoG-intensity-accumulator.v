d82e029474cfdd18bd3938ca0b6d20ba2c3fc1c4
Michael Grillo <mjg397@cornell.edu>
2026-04-17 21:52:52 -0400

    i think my code is pretty
    Added full_frame_complete output signal and corrected logic for subapeture indexing and pixel counting.

--- verilog/TCoG-intensity-accumulator.v
+++ verilog/TCoG-intensity-accumulator.v
@@ -7,6 +7,7 @@ module TCoG_intensity_accumulator #(
   input wire reset,
   input wire valid,
   input wire [7:0] data_in,
+  output reg full_frame_complete;
   output reg [$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] subapetures_completed // indexed with i=1 corresponding to centroid 0, 0 completed
   output reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] intensity,
   output reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] x_intensity,
@@ -15,53 +16,83 @@ module TCoG_intensity_accumulator #(
   output reg [$clog2(NUM_SUBAPETURES_SQRT)-1:0] subap_row
 );
 
+wire [$clog2(NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT)-1:0] subap_idx;
+wire last_pixel_h = (count_pixel_h == NUM_PIXELS_SUBAPETURE-SQRT-1);
+wire last_pixel_v = (count_pixel_v == NUM_PIXELS_SUBAPETURE_SQRT-1);
+wire last_subap_col = (subap_col == NUM_SUBAPETURE_SQRT-1);
+wire last_subap_row = (subap_row == NUM_SUBAPETURE_SQRT-1);
+
 reg [$clog2(NUM_SUBAPETURES_SQRT)-1:0] subap_col;
 reg [$clog2(NUM_SUBAPETURES_SQRT)-1:0] subap_row;
 reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] i   [NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1:0];
 reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] x_i [NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1:0];
 reg [8+$clog2(NUM_PIXELS_SUBAPETURE_SQRT)-1:0] y_i [NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1:0];
 
-assign subap_idx = subap_row * NUM_SUBAP_X + subap_col;
+assign subap_idx = (subap_row * NUM_SUBAPETURES_SQRT + subap_col);
 
 //  TCoG indexing control logic
   always @(posedge clk) begin
     if (reset) begin
-      subapetures_completed <= 0
-      intensity <= 0;
-      x_intensity <= 0;
-      y_intensity <= 0;
+      subapetures_completed <= 0;
       subap_col <= 0;
       subap_row <= 0;
     end
-    else begin
-      if valid begin
+    else if (valid) begin
+      full_frame_complete <= 0;
+
+      if (last_pixel_h) begin
+        count_pixel_h <= 0;
+      end else begin
         count_pixel_h <= count_pixel_h + 1;
-        if (count_pixel_h == NUM_PIXELS_SUBAPETURE) begin
-          count_pixel_h <= 0;
-          subap_col <= subap_col + 1;
-          if (subap_col_v == NUM_SUBAPETURE) begin
-            subap_col <= 0;
-            count_pixel_v <= count_pixel_v + 1;
-            if (subap_pixel_v == NUM_PIXELS_SUBAPETURE) begin
-              subap_row <= subap_row + 1;
+      end
+
+      if (last_pixel_h) begin
+        if (last_pixel_v) begin
+          count_pixel_v <= 0;
+        end else begin
+          count_pixel_v <= count_pixel_v + 1;
+        end
+      end
+
+      if (last_pixel_h && last_pixel_v) begin
+          if (last_subap_col)
               subap_col <= 0;
-              if (subap_row == NUM_SUBAPETURE && subap_col == NUM_SUBAPEUTRE && count_pixel_h == NUM_PIXELS_SUBAPEUTRE && count_pixel_v == NUM_PIXELS_SUBAPETURE) begin
-                complete_full_frame <= 1;
-                
-              end
-            end
-          end
+          else
+              subap_col <= subap_col + 1;
+       end
+
+      if (last_pixel_h && last_pixel_v && last_subap_col) begin
+        if (last_subap_row) begin
+          subap_row <= 0;
+          full_frame_complete <= 1;
+        end else begin
+          subap_row <= subap_row + 1;
         end
       end
     end
   end
+  
+  // Completed subapeture indication
+  always @(posedge clk) begin
+    if (reset) begin
+      intensity <= 0;
+      x_intensity <= 0;
+      y_intensity <= 0;
+    end else begin
+      if (last_pixel_h && last_pixel_v) begin
+        subapetures_completed <= subapetures_completed + 1;
+        intensity <= i[subap_idx];
+        x_intensity <= x_i[subap_idx];
+        y_intensity <= y_i[subap_idx];
+      end
+    end
+  end
 
-  // completion logic
 
   // Intensity accumulation of valid pixel data
   always @(posedge clk) begin
     if (reset) begin
-      for (i = 0; i < NUM_SUBAPETURES*NUM_SUBAPETURES-1; i = i + 1) begin
+      for (i = 0; i < NUM_SUBAPETURES_SQRT*NUM_SUBAPETURES_SQRT-1; i = i + 1) begin
               i[i]   <= 8'b0; 
               x_i[i] <= 8'b0;
               y_i[i] <= 8'b0;
