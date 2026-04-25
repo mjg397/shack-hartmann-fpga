44143134b60305cc4f3d65bf6bfeb9a534b68842
Michael Grillo <mjg397@cornell.edu>
2026-04-25 14:20:11 -0400

    im getting there soon. ece formal was cool, stephen and md faded.
    Refactor calculation logic to use unsigned multiplication for centroids and slopes.

--- verilog/reference-calculation.v
+++ verilog/reference-calculation.v
@@ -10,30 +10,50 @@ module reference_calculation (
   output reg [26:0] y_centroid,
   output reg [26:0] x_slope,
   output reg [26:0] y_slope
+  //output reg new_subapeture
 );
   
-localparam integer SCALE = 8388608; // 2^23 for 4.23 fixed point
-localparam x_ref = 7.5 * SCALE;
-localparam y_ref = 7.5 * SCALE;
+  localparam integer SCALE = 8388608; // 2^23 for 4.23 fixed point
+  localparam x_ref = 7.5 * SCALE;
+  localparam y_ref = 7.5 * SCALE;
 
-always @(posedge clk) begin
-  if (reset) begin
+  wire [26:0] x_centroid_mult;
+  wire [26:0] y_centroid_mult;
+
+  unsigned_mult x_mult(
+    .out(x_centroid_mult),
+    .a(x_intensity),
+    .b(rec_intensity)
+  );
+  
+  signed_mult y_mult(
+    .out(y_centroid_mult),
+    .a(y_intensity),
+    .b(rec_intensity)
+  );
+
+
+  always @(posedge clk) begin
+  if (rst) begin
     x_slope <= 0;
     y_slope <= 0;
-    slope_vector <= 0;
-    subapetures_completed <= 0;
+    x_centroid <= 0;
+    y_centroid <= 0;
   end else begin
-    x_centroid <= signed_mult(x_intensity, rec_intensity);
-    y_centroid <= signed_mult(y_intensity, rec_intensity);
-    x_slopes <= x_centroid - x_ref;
-    y_slopes <= y_centroid - y_ref;
+    x_centroid <= x_centroid_mult;
+    y_centroid <= y_centroid_mult;
+    x_slope <= x_centroid - x_ref;
+    y_slope <= y_centroid - y_ref;
   end
+ end
 endmodule
 
-module signed_mult (out, a, b);
-  output signed [26:0] out;
-  input signed  [19:0] a;  // in this case 20.0
-  input signed  [26:0] b;  // in this case 0.27
+
+module unsigned_mult (
+  output signed [26:0] out,
+  input signed  [19:0] a,  // in this case 20.0
+  input signed  [26:0] b  // in this case 0.27
+  );
   // intermediate full bit length
   wire signed [46:0] mult_out;
   assign mult_out = a * b;
